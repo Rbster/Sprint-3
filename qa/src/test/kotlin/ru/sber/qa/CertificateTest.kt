b0ca213f2d212e77455b818a665b1b0a62a96e46
Rbster <Rbster@yandex.ru>
2021-08-31 00:55:38 +0300

    started qa

---
+++ qa/src/test/kotlin/ru/sber/qa/CertificateTest.kt
@@ -0,0 +1,21 @@
+package ru.sber.qa
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CertificateTest {
+
+    @Test
+    fun getCertificateRequest() {
+
+    }
+
+    @Test
+    fun getProcessedBy() {
+    }
+
+    @Test
+    fun getData() {
+    }
+}
